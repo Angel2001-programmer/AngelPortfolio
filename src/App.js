1f7761f949e9c0b7014f24ee62cde9d04a24a067
Angel2001-programmer <angelwitchell2001@gmail.com>
2024-01-21 03:51:17 +0000

    project setup.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 className='text-left font-bold text-[50px] ml-3'>Project Setup :D</h1>
+      <h1 className='text-left font-bold text-[50px] ml-3'>
+        TailWindCSS installed. ✅
+      </h1>
+      <h1 className='text-left font-bold text-[50px] ml-3'>
+        React Router installed. ✅
+      </h1>
     </div>
   );
 }
